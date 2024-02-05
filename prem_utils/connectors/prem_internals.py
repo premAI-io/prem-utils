c2449a062d64ce783f65469ceb28dceca7ba51b2
Anindyadeep <anindyadeep@pop-os.localdomain>
2024-02-05 19:22:54 +0530

    Added post process function to todo

--- prem_utils/connectors/prem_internals.py
+++ prem_utils/connectors/prem_internals.py
@@ -1,59 +1,61 @@
+from typing import Any
+
 import requests
-from typing import Optional, Any, List
 
 from prem_utils import errors
 from prem_utils.connectors.base import BaseConnector
- 
+
 
 class PremInternalConnectors(BaseConnector):
-    def __init__(self, api_key: str, prompt_template: Optional[str] = None) -> None:
+    def __init__(self, api_key: str, prompt_template: str | None = None) -> None:
         super().__init__(prompt_template=prompt_template)
         self._api_key = api_key
-    
-    
+
     def parse_chunk(self, chunk) -> dict[str, Any]:
         # Todo: Need to understand how it is used.
         pass
-    
-    def build_messages(self, messages: List[dict]) -> List[str]:
-        # Todo: Whether it can be used in current providers 
+
+    def build_messages(self, messages: list[dict]) -> list[str]:
+        # Todo: Whether it can be used in current providers
         pass
-    
+
+    def preprocess_messages(self, messages):
+        # Todo: Need to understand whether to use it and how to use it.
+        pass
+
     def chat_completion(
-        self, 
+        self,
         model_name: str,
-        messages: list[dict[str]], 
-        max_tokens: int, 
-        temperature: Optional[float] = 1.0,
-        top_p: Optional[float] = 1.0
-        ):
-        assert model_name in ['phi-1-5', 'phi-2', 'tinyllama', 'mamba-chat'], \
-            ValueError("Models other than 'phi-1-5', 'phi-2', 'tinyllama', 'mamba-chat' are not supported") 
-        
+        messages: list[dict[str]],
+        max_tokens: int,
+        temperature: float | None = 1.0,
+        top_p: float | None = 1.0,
+    ):
+        assert model_name in ["phi-1-5", "phi-2", "tinyllama", "mamba-chat"], ValueError(
+            "Models other than 'phi-1-5', 'phi-2', 'tinyllama', 'mamba-chat' are not supported"
+        )
+
         # this is how msgs look like: [{'role': 'user', 'content': ...}]
-        if model_name == 'mamba-chat':
+        if model_name == "mamba-chat":
             _base_url = "https://mamba.compute.premai.io/v1/chat/completions"
             data = {
-                'model': model_name, 
-                'messages': messages,
-                'max_length': max_tokens,
-                'temperature': temperature,
-                'top_p': top_p
+                "model": model_name,
+                "messages": messages,
+                "max_length": max_tokens,
+                "temperature": temperature,
+                "top_p": top_p,
             }
         else:
-            _base_url = f"https://{model_name}.compute.premai.io/mii/default" 
+            _base_url = f"https://{model_name}.compute.premai.io/mii/default"
             data = {
-                'prompts': [message['content'] for message in messages], 
-                'max_length': max_tokens,
-                'temperature': temperature,
-                'top_p': top_p
+                "prompts": [message["content"] for message in messages],
+                "max_length": max_tokens,
+                "temperature": temperature,
+                "top_p": top_p,
             }
-            
-        _headers = {
-            "Content-Type": "application/json",
-            "Authorization": f"Bearer {self._api_key}"
-        }
-                
+
+        _headers = {"Content-Type": "application/json", "Authorization": f"Bearer {self._api_key}"}
+
         try:
             response = requests.post(_base_url, json=data, headers=_headers)
         except (
@@ -72,5 +74,5 @@ class PremInternalConnectors(BaseConnector):
             errors.PremProviderAPIConnectionError,
         ) as error:
             raise error
-        
+
         return response.text
