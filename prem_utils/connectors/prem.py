cc630919843e1433de0ba9bb1defcd621d9ace8d
Anindyadeep <anindyadeep@pop-os.localdomain>
2024-02-21 13:16:38 +0530

    Moved default urls to PremConnector class and delete the seperate default urls file

--- prem_utils/connectors/prem.py
+++ prem_utils/connectors/prem.py
@@ -5,12 +5,33 @@ import requests
 
 from prem_utils import errors
 from prem_utils.connectors.base import BaseConnector
-from prem_utils.default_urls import DEFAULT_PREM_URLS
 
 
 class PremConnector(BaseConnector):
     def __init__(self, api_key: str, prompt_template: str | None = None) -> None:
         super().__init__(prompt_template=prompt_template)
+        self.url_mappings = {
+            "mamba": {
+                "generation": "https://premai-io--generate-mamba-dev.modal.run",
+                "completion": "https://premai-io--completion-mamba-dev.modal.run",
+            },
+            "phi2": {
+                "generation": "https://premai-io--generate-phi2-dev.modal.run",
+                "completion": "https://premai-io--completion-phi2-dev.modal.run",
+            },
+            "phi1.5": {
+                "generation": "https://premai-io--generate-phi1-5-dev.modal.run",
+                "completion": "https://premai-io--completion-phi1-5-dev.modal.run",
+            },
+            "stable_lm2": {
+                "generation": "https://premai-io--generate-stable-lm2-zephyr-dev.modal.run",
+                "completion": "https://premai-io--completion-stable-lm2-zephyr-dev.modal.run",
+            },
+            "tinyllama": {
+                "generation": "https://premai-io--generate-tinyllama-dev.modal.run",
+                "completion": "https://premai-io--completion-tinyllama-dev.modal.run",
+            },
+        }
         self._api_key = api_key
         self._prem_errors = (
             errors.PremProviderAPIErrror,
@@ -51,7 +72,7 @@ class PremConnector(BaseConnector):
         for message in messages:
             data["prompt"] = message["content"]
             try:
-                response = requests.post(DEFAULT_PREM_URLS[model]["completion"], json=data, timeout=600, stream=True)
+                response = requests.post(self.url_mappings[model]["completion"], json=data, timeout=600, stream=True)
                 if response.status_code == 200:
                     for line in response.iter_lines():
                         token_to_sent = {"status": 200, "content": line.decode("utf-8")}
@@ -74,7 +95,7 @@ class PremConnector(BaseConnector):
         for message in messages:
             data["prompt"] = message["content"]
             try:
-                response = requests.post(DEFAULT_PREM_URLS[model]["generation"], json=data, timeout=600)
+                response = requests.post(self.url_mappings[model]["generation"], json=data, timeout=600)
                 responses.append(response.text)
 
             except self._prem_errors as e:
