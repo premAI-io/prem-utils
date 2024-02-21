4400b497ab2070007aa920f3dbab41f3d6c4a762
Anindyadeep <anindyadeep@pop-os.localdomain>
2024-02-21 12:58:22 +0530

    Added default_urls to store different default urls

---
+++ prem_utils/default_urls.py
@@ -0,0 +1,22 @@
+DEFAULT_PREM_URLS = {
+    "mamba": {
+        "generation": "https://premai-io--generate-mamba-dev.modal.run",
+        "completion": "https://premai-io--completion-mamba-dev.modal.run",
+    },
+    "phi2": {
+        "generation": "https://premai-io--generate-phi2-dev.modal.run",
+        "completion": "https://premai-io--completion-phi2-dev.modal.run",
+    },
+    "phi1.5": {
+        "generation": "https://premai-io--generate-phi1-5-dev.modal.run",
+        "completion": "https://premai-io--completion-phi1-5-dev.modal.run",
+    },
+    "stable_lm2": {
+        "generation": "https://premai-io--generate-stable-lm2-zephyr-dev.modal.run",
+        "completion": "https://premai-io--completion-stable-lm2-zephyr-dev.modal.run",
+    },
+    "tinyllama": {
+        "generation": "https://premai-io--generate-tinyllama-dev.modal.run",
+        "completion": "https://premai-io--completion-tinyllama-dev.modal.run",
+    },
+}
