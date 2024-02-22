8c2b876d77027b3186b48840403082e6d0eba0d0
Anindyadeep <anindyadeep@pop-os.localdomain>
2024-02-23 00:13:13 +0530

    Feat: Added OpenAIBaseConnector
    Reason of adding this is, some of the providers like perplexity, deepinfra, openrouter, Anyscale
    uses openai directly, so chat_completion of OpenAI is also compatible with them. This reduces redundancy
    at the same time having provider specific class

--- prem_utils/connectors/base.py
+++ prem_utils/connectors/base.py
@@ -1,8 +1,42 @@
+import json
 from collections.abc import Sequence
 
 import tiktoken
+from openai import (
+    APIConnectionError,
+    APIError,
+    APIResponseValidationError,
+    APIStatusError,
+    APITimeoutError,
+    AuthenticationError,
+    BadRequestError,
+    ConflictError,
+    InternalServerError,
+    NotFoundError,
+    OpenAI,
+    PermissionDeniedError,
+    RateLimitError,
+    UnprocessableEntityError,
+)
 
 from prem_utils import errors
+
+
+def get_provider_blob(provider_name: str) -> dict:
+    try:
+        with open("./prem_utils/models.json") as file:
+            data = json.load(file)
+            for connector in data["connectors"]:
+                if connector["provider"] == provider_name:
+                    return connector
+            print(f"No data found for provider: {provider_name}")
+            return None
+    except FileNotFoundError:
+        print("JSON file not found.")
+        return None
+    except json.JSONDecodeError:
+        print("Invalid JSON format.")
+        return None
 
 
 class BaseConnector:
@@ -18,6 +52,9 @@ class BaseConnector:
             429: errors.PremProviderRateLimitError,
             409: errors.PremProviderConflictError,
         }
+
+    def list_models(self):
+        raise NotImplementedError
 
     def apply_prompt_template(self, messages):
         prompt = self.prompt_template
@@ -78,3 +115,125 @@ class BaseConnector:
 
     def get_finetuning_job(self, job_id) -> dict[str, any]:
         raise NotImplementedError
+
+
+# The reason we have a base class for OpenAI is because this can easily integrate
+# providers which provides services through Open AI's SDK
+class OpenAIBaseConnector(BaseConnector):
+    def __init__(self, api_key: str = None, base_url: str = None, prompt_template: str = None):
+        super().__init__(prompt_template=prompt_template)
+        self.exception_mapping = {
+            APIError: errors.PremProviderAPIErrror,
+            PermissionDeniedError: errors.PremProviderPermissionDeniedError,
+            UnprocessableEntityError: errors.PremProviderUnprocessableEntityError,
+            InternalServerError: errors.PremProviderInternalServerError,
+            AuthenticationError: errors.PremProviderAuthenticationError,
+            BadRequestError: errors.PremProviderBadRequestError,
+            NotFoundError: errors.PremProviderNotFoundError,
+            RateLimitError: errors.PremProviderRateLimitError,
+            APIResponseValidationError: errors.PremProviderAPIResponseValidationError,
+            ConflictError: errors.PremProviderConflictError,
+            APIStatusError: errors.PremProviderAPIStatusError,
+            APITimeoutError: errors.PremProviderAPITimeoutError,
+            APIConnectionError: errors.PremProviderAPIConnectionError,
+        }
+        if base_url is not None:
+            self.client = OpenAI(api_key=api_key, base_url=base_url)
+        else:
+            self.client = OpenAI(api_key=api_key)
+
+    def chat_completion(
+        self,
+        model: str,
+        messages: list[dict[str]],
+        max_tokens: int = None,
+        frequency_penalty: float = 0,
+        log_probs: int = None,
+        logit_bias: dict[str, float] = None,
+        presence_penalty: float = 0,
+        seed: int | None = None,
+        stop: str | list[str] = None,
+        stream: bool = False,
+        temperature: float = 1,
+        top_p: float = 1,
+        tools: list[dict[str]] = None,
+        tool_choice: dict = None,
+    ):
+        if self.prompt_template is not None:
+            messages = self.apply_prompt_template(messages)
+
+        # NOTE custom logic for providers who don't have
+        # their sdk, but they use direclty OpenAI python client.
+        if "deepinfra" in model:
+            model = model.replace("deepinfra/", "", 1)
+            max_tokens = max_tokens or 1024
+
+        try:
+            response = self.client.chat.completions.create(
+                model=model,
+                messages=messages,
+                tools=tools,
+                tool_choice=tool_choice,
+                stream=stream,
+                max_tokens=max_tokens,
+                frequency_penalty=frequency_penalty,
+                presence_penalty=presence_penalty,
+                seed=seed,
+                stop=stop,
+                temperature=temperature,
+                top_p=top_p,
+            )
+        except (
+            NotFoundError,
+            APIResponseValidationError,
+            ConflictError,
+            APIStatusError,
+            APITimeoutError,
+            RateLimitError,
+            BadRequestError,
+            APIConnectionError,
+            AuthenticationError,
+            InternalServerError,
+            PermissionDeniedError,
+            UnprocessableEntityError,
+        ) as error:
+            custom_exception = self.exception_mapping.get(type(error), errors.PremProviderError)
+            raise custom_exception(error, provider="openai", model=model, provider_message=str(error))
+
+        if stream:
+            return response
+        plain_response = {
+            "id": response.id,
+            "choices": [
+                {
+                    "finish_reason": choice.finish_reason,
+                    "index": choice.index,
+                    "message": {
+                        "content": choice.message.content,
+                        "role": choice.message.role,
+                        "tool_calls": [
+                            {
+                                "id": tool_call.id,
+                                "type": tool_call.type,
+                                "function": {
+                                    "name": tool_call.function.name,
+                                    "arguments": tool_call.function.arguments,
+                                },
+                            }
+                            for tool_call in choice.message.tool_calls or []
+                        ],
+                    },
+                }
+                for choice in response.choices
+            ],
+            "created": response.created,
+            "model": response.model,
+            "provider_name": "OpenAI",
+            "provider_id": "openai",
+            "usage": {
+                "completion_tokens": response.usage.completion_tokens,
+                "prompt_tokens": response.usage.prompt_tokens,
+                "total_tokens": response.usage.total_tokens,
+            },
+        }
+        return plain_response
