76727d266cd6df1ba591ae7e3a63af1f06b840c8
Anindyadeep <anindyadeep@pop-os.localdomain>
2024-02-23 00:20:37 +0530

    Refactor: OpenAIConnector uses OpenAIBaseConnector and also added list_models function that will print the available models and other related info in a table

--- prem_utils/connectors/openai.py
+++ prem_utils/connectors/openai.py
@@ -4,7 +4,6 @@ from collections.abc import Sequence
 
 from openai import (
     APIConnectionError,
-    APIError,
     APIResponseValidationError,
     APIStatusError,
     APITimeoutError,
@@ -13,38 +12,44 @@ from openai import (
     ConflictError,
     InternalServerError,
     NotFoundError,
-    OpenAI,
     PermissionDeniedError,
     RateLimitError,
     UnprocessableEntityError,
 )
+from tabulate import tabulate as tab
 
 from prem_utils import errors
-from prem_utils.connectors.base import BaseConnector
+from prem_utils.connectors.base import OpenAIBaseConnector, get_provider_blob
 
 
-class OpenAIConnector(BaseConnector):
+class OpenAIConnector(OpenAIBaseConnector):
     def __init__(self, api_key: str = None, base_url: str = None, prompt_template: str = None):
-        super().__init__(prompt_template=prompt_template)
-        self.exception_mapping = {
-            APIError: errors.PremProviderAPIErrror,
-            PermissionDeniedError: errors.PremProviderPermissionDeniedError,
-            UnprocessableEntityError: errors.PremProviderUnprocessableEntityError,
-            InternalServerError: errors.PremProviderInternalServerError,
-            AuthenticationError: errors.PremProviderAuthenticationError,
-            BadRequestError: errors.PremProviderBadRequestError,
-            NotFoundError: errors.PremProviderNotFoundError,
-            RateLimitError: errors.PremProviderRateLimitError,
-            APIResponseValidationError: errors.PremProviderAPIResponseValidationError,
-            ConflictError: errors.PremProviderConflictError,
-            APIStatusError: errors.PremProviderAPIStatusError,
-            APITimeoutError: errors.PremProviderAPITimeoutError,
-            APIConnectionError: errors.PremProviderAPIConnectionError,
-        }
-        if base_url is not None:
-            self.client = OpenAI(api_key=api_key, base_url=base_url)
-        else:
-            self.client = OpenAI(api_key=api_key)
+        super().__init__(api_key=api_key, base_url=base_url, prompt_template=prompt_template)
+        self._errors = (
+            NotFoundError,
+            APIResponseValidationError,
+            ConflictError,
+            APIStatusError,
+            APITimeoutError,
+            RateLimitError,
+            BadRequestError,
+            APIConnectionError,
+            AuthenticationError,
+            InternalServerError,
+            PermissionDeniedError,
+            UnprocessableEntityError,
+        )
+
+    def list_models(self):
+        provider_blob = get_provider_blob("openai")
+        models = provider_blob["models"]
+
+        # Convert models data into a list of lists for tabulate
+        table_data = [[model.get(key, "") for key in models[0].keys()] for model in models]
+
+        # Insert a header row with the keys of the models dictionary
+        table_data.insert(0, models[0].keys())
+        print(tab(table_data, headers="firstrow", tablefmt="grid"))
 
     def parse_chunk(self, chunk):
         return {
@@ -64,102 +69,6 @@ class OpenAIConnector(BaseConnector):
             ],
         }
 
-    def chat_completion(
-        self,
-        model: str,
-        messages: list[dict[str]],
-        max_tokens: int = None,
-        frequency_penalty: float = 0,
-        log_probs: int = None,
-        logit_bias: dict[str, float] = None,
-        presence_penalty: float = 0,
-        seed: int | None = None,
-        stop: str | list[str] = None,
-        stream: bool = False,
-        temperature: float = 1,
-        top_p: float = 1,
-        tools: list[dict[str]] = None,
-        tool_choice: dict = None,
-    ):
-        if self.prompt_template is not None:
-            messages = self.apply_prompt_template(messages)
-
-        # NOTE custom logic for providers who don't have
-        # their sdk, but they use direclty OpenAI python client.
-        if "deepinfra" in model:
-            model = model.replace("deepinfra/", "", 1)
-            max_tokens = max_tokens or 1024
-
-        try:
-            response = self.client.chat.completions.create(
-                model=model,
-                messages=messages,
-                tools=tools,
-                tool_choice=tool_choice,
-                stream=stream,
-                max_tokens=max_tokens,
-                frequency_penalty=frequency_penalty,
-                presence_penalty=presence_penalty,
-                seed=seed,
-                stop=stop,
-                temperature=temperature,
-                top_p=top_p,
-            )
-        except (
-            NotFoundError,
-            APIResponseValidationError,
-            ConflictError,
-            APIStatusError,
-            APITimeoutError,
-            RateLimitError,
-            BadRequestError,
-            APIConnectionError,
-            AuthenticationError,
-            InternalServerError,
-            PermissionDeniedError,
-            UnprocessableEntityError,
-        ) as error:
-            custom_exception = self.exception_mapping.get(type(error), errors.PremProviderError)
-            raise custom_exception(error, provider="openai", model=model, provider_message=str(error))
-
-        if stream:
-            return response
-        plain_response = {
-            "id": response.id,
-            "choices": [
-                {
-                    "finish_reason": choice.finish_reason,
-                    "index": choice.index,
-                    "message": {
-                        "content": choice.message.content,
-                        "role": choice.message.role,
-                        "tool_calls": [
-                            {
-                                "id": tool_call.id,
-                                "type": tool_call.type,
-                                "function": {
-                                    "name": tool_call.function.name,
-                                    "arguments": tool_call.function.arguments,
-                                },
-                            }
-                            for tool_call in choice.message.tool_calls or []
-                        ],
-                    },
-                }
-                for choice in response.choices
-            ],
-            "created": response.created,
-            "model": response.model,
-            "provider_name": "OpenAI",
-            "provider_id": "openai",
-            "usage": {
-                "completion_tokens": response.usage.completion_tokens,
-                "prompt_tokens": response.usage.prompt_tokens,
-                "total_tokens": response.usage.total_tokens,
-            },
-        }
-        return plain_response
-
     def embeddings(
         self,
         model: str,
@@ -169,20 +78,7 @@ class OpenAIConnector(BaseConnector):
     ):
         try:
             response = self.client.embeddings.create(model=model, input=input)
-        except (
-            NotFoundError,
-            APIResponseValidationError,
-            ConflictError,
-            APIStatusError,
-            APITimeoutError,
-            RateLimitError,
-            BadRequestError,
-            APIConnectionError,
-            AuthenticationError,
-            InternalServerError,
-            PermissionDeniedError,
-            UnprocessableEntityError,
-        ) as error:
+        except self._errors as error:
             custom_exception = self.exception_mapping.get(type(error), errors.PremProviderError)
             raise custom_exception(error, provider="openai", model=model, provider_message=str(error))
         return {
@@ -222,20 +118,7 @@ class OpenAIConnector(BaseConnector):
 
         try:
             response = self.client.fine_tuning.jobs.create(**fine_tuning_params)
-        except (
-            NotFoundError,
-            APIResponseValidationError,
-            ConflictError,
-            APIStatusError,
-            APITimeoutError,
-            RateLimitError,
-            BadRequestError,
-            APIConnectionError,
-            AuthenticationError,
-            InternalServerError,
-            PermissionDeniedError,
-            UnprocessableEntityError,
-        ) as error:
+        except self._errors as error:
             custom_exception = self.exception_mapping.get(type(error), errors.PremProviderError)
             raise custom_exception(error, provider="openai", model=model, provider_message=str(error))
         return response.id
@@ -285,20 +168,7 @@ class OpenAIConnector(BaseConnector):
 
             try:
                 response = self.client.files.create(file=temp_file.file, purpose="fine-tune")
-            except (
-                NotFoundError,
-                APIResponseValidationError,
-                ConflictError,
-                APIStatusError,
-                APITimeoutError,
-                RateLimitError,
-                BadRequestError,
-                APIConnectionError,
-                AuthenticationError,
-                InternalServerError,
-                PermissionDeniedError,
-                UnprocessableEntityError,
-            ) as error:
+            except self._errors as error:
                 custom_exception = self.exception_mapping.get(type(error), errors.PremProviderError)
                 raise custom_exception(error, provider="openai", model=None, provider_message=str(error))
         return response.id
@@ -323,20 +193,7 @@ class OpenAIConnector(BaseConnector):
                 style=style,
                 response_format=response_format,
             )
-        except (
-            NotFoundError,
-            APIResponseValidationError,
-            ConflictError,
-            APIStatusError,
-            APITimeoutError,
-            RateLimitError,
-            BadRequestError,
-            APIConnectionError,
-            AuthenticationError,
-            InternalServerError,
-            PermissionDeniedError,
-            UnprocessableEntityError,
-        ) as error:
+        except self._errors as error:
             custom_exception = self.exception_mapping.get(type(error), errors.PremProviderError)
             raise custom_exception(error, provider="openai", model=model, provider_message=str(error))
         return {"created": None, "data": [{"url": image.url, "b64_json": image.b64_json} for image in response.data]}
