eb3c8d2d164e33ef2e6b71f936468aeebfe74085
Anindyadeep <anindyadeep@pop-os.localdomain>
2024-02-23 00:25:04 +0530

    Refactor: e2e now supports CLI so that we do not need to call all the models everytime for testing purposes

--- e2e.py
+++ e2e.py
@@ -1,3 +1,4 @@
+import argparse
 import json
 import logging
 import os
@@ -20,7 +21,6 @@ from prem_utils.connectors import (
 )
 
 logger = logging.getLogger(__name__)
-
 load_dotenv()
 
 
@@ -35,89 +35,130 @@ def load_models_file():
         return None
 
 
-def main():
-    models_file = load_models_file()
-
-    for connector in models_file["connectors"]:
-        if connector["provider"] == "anthropic":
-            connector_object = anthropic.AnthropicConnector(api_key=os.environ["ANTHROPIC_API_KEY"])
-        elif connector["provider"] == "azure":
-            connector_object = azure.AzureOpenAIConnector(
-                api_key=os.environ["AZURE_OPENAI_API_KEY"],
-                base_url=os.environ["AZURE_OPENAI_BASE_URL"],
-            )
-        elif connector["provider"] == "cohere":
-            connector_object = cohere.CohereConnector(api_key=os.environ["COHERE_API_KEY"])
-        elif connector["provider"] == "fireworksai":
-            connector_object = fireworksai.FireworksAIConnector(api_key=os.environ["FIREWORKS_AI_API_KEY"])
-        elif connector["provider"] == "octoai":
-            connector_object = octoai.OctoAIConnector(api_key=os.environ["OCTO_AI_API_KEY"])
-        elif connector["provider"] == "openai":
-            connector_object = openai.OpenAIConnector(api_key=os.environ["OPENAI_API_KEY"])
-        elif connector["provider"] == "replicate":
-            connector_object = replicate.ReplicateConnector(api_key=os.environ["REPLICATE_API_KEY"])
-        elif connector["provider"] == "together":
-            connector_object = together.TogetherConnector(api_key=os.environ["TOGETHER_AI_API_KEY"])
-        elif connector["provider"] == "cloudflare":
-            connector_object = cloudflare.CloudflareConnector(
-                api_key=os.environ["CLOUDFLARE_API_KEY"],
-                account_id=os.environ["CLOUDFLARE_ACCOUNT_ID"],
-            )
-        elif connector["provider"] == "mistralai":
-            connector_object = mistral.MistralConnector(api_key=os.environ["MISTRAL_AI_API_KEY"])
-        elif connector["provider"] == "deepinfra":
-            connector_object = openai.OpenAIConnector(
-                api_key=os.environ["DEEP_INFRA_API_KEY"],
-                base_url="https://api.deepinfra.com/v1/openai",
-            )
-        elif connector["provider"] == "prem":
-            connector_object = prem.PremConnector(api_key=os.environ["PREMAI_BEARER_TOKEN"])
-        else:
-            print(f"No connector for {connector['provider']}")
-
-        text2text_models = [model for model in connector["models"] if model["model_type"] == "text2text"]
-        text2vector_models = [model for model in connector["models"] if model["model_type"] == "text2vector"]
-        text2image_models = [model for model in connector["models"] if model["model_type"] == "text2image"]
-
-        if len(text2text_models) > 0:
-            model_object = text2text_models[0]
-
-            parameters = {}
-            parameters["model"] = model_object["slug"]
-            messages = [{"role": "user", "content": "Hello, how is it going?"}]
-            messages.append({"role": "system", "content": "Behave like Rick Sanchez."})
-            parameters["messages"] = messages
-
-            print(f"Testing model {model_object['slug']} from {connector['provider']} connector \n\n\n")
-            response = connector_object.chat_completion(stream=False, **parameters)
-            print(f"\n\n\n Model {model_object['slug']} succeeed 🚀 \n\n\n")
-
-            response = connector_object.chat_completion(stream=True, **parameters)
-            for text in response:
-                print(connector_object.parse_chunk(text))
-            print(f"\n\n\n Model {model_object['slug']} succeeed with streaming 🚀 \n\n\n")
-
-        if len(text2image_models) > 0:
-            model_object = text2image_models[0]
-
-            parameters = {}
-            parameters["model"] = model_object["slug"]
-            parameters["prompt"] = "A cute baby sea otter"
-            parameters["size"] = "1024x1024"
-            parameters["n"] = 1
-
-            print(f"Testing model {model_object['slug']} from {connector['provider']} connector \n\n\n")
-            response = connector_object.generate_image(**parameters)
-            print(f"\n\n\n Model {model_object['slug']} succeeed 🚀 \n\n\n")
-
-        if len(text2vector_models) > 0:
-            model_object = text2vector_models[0]
-
-            input = "Hello, how is it going?"
-            print(f"Testing model {model_object['slug']} from {connector['provider']} connector")
-            response = connector_object.embeddings(model=model_object["slug"], input=input)
-            print(f"Embeddings: {len(response['data'][0])}")
-            print(f"Model {model_object['slug']} succeeed 🚀 \n\n\n")
+def get_provider_blob(provider_name: str) -> dict:
+    try:
+        with open("./prem_utils/models.json") as file:
+            data = json.load(file)
+            for connector in data["connectors"]:
+                if connector["provider"] == provider_name:
+                    return connector
+            print(f"No data found for provider: {provider_name}")
+            return None
+    except FileNotFoundError:
+        print("JSON file not found.")
+        return None
+    except json.JSONDecodeError:
+        print("Invalid JSON format.")
+        return None
 
 
-main()
+def run_single_connector(connector_name: str) -> None:
+    connectors_mapping = load_models_file()["connectors"]
+    all_connectors = [connector["provider"] for connector in connectors_mapping]
+    assert connector_name in all_connectors, ValueError(f"Connector: {connector_name} does not exist")
+    connector = get_provider_blob(provider_name=connector_name)
+
+    connector_class_mapping = {
+        "anthropic": (anthropic.AnthropicConnector, "ANTHROPIC_API_KEY"),
+        "azure": (azure.AzureOpenAIConnector, "AZURE_OPENAI_API_KEY"),
+        "cohere": (cohere.CohereConnector, "COHERE_API_KEY"),
+        "fireworksai": (fireworksai.FireworksAIConnector, "FIREWORKS_AI_API_KEY"),
+        "octoai": (octoai.OctoAIConnector, "OCTO_AI_API_KEY"),
+        "openai": (openai.OpenAIConnector, "OPENAI_API_KEY"),
+        "replicate": (replicate.ReplicateConnector, "REPLICATE_API_KEY"),
+        "together": (together.TogetherConnector, "TOGETHER_AI_API_KEY"),
+        "cloudflare": (cloudflare.CloudflareConnector, "CLOUDFLARE_API_KEY"),
+        "mistralai": (mistral.MistralConnector, "MISTRAL_AI_API_KEY"),
+        "prem": (prem.PremConnector, "PREMAI_BEARER_TOKEN"),
+        "deepinfra": (openai.OpenAIConnector, "DEEP_INFRA_API_KEY"),
+    }
+
+    if connector_name == "deepinfra":
+        connector_object = connector_class_mapping[connector_name][0](
+            api_key=os.environ[connector_class_mapping[connector_name][1]],
+            base_url="https://api.deepinfra.com/v1/openai",
+        )
+    elif connector_name == "cloudflare":
+        connector_object = connector_class_mapping[connector_name][0](
+            api_key=os.environ[connector_class_mapping[connector_name][1]],
+            account_id=os.environ["CLOUDFLARE_ACCOUNT_ID"],
+        )
+    elif connector_name == "azure":
+        connector_object = connector_class_mapping[connector_name][0](
+            api_key=os.environ[connector_class_mapping[connector_name][1]],
+            base_url=os.environ["AZURE_OPENAI_BASE_URL"],
+        )
+    else:
+        connector_object = connector_class_mapping[connector_name][0](
+            api_key=os.environ[connector_class_mapping[connector_name][1]]
+        )
+
+    text2text_models = [model for model in connector["models"] if model["model_type"] == "text2text"]
+    text2vector_models = [model for model in connector["models"] if model["model_type"] == "text2vector"]
+    text2image_models = [model for model in connector["models"] if model["model_type"] == "text2image"]
+
+    if len(text2text_models) > 0:
+        model_object = text2text_models[0]
+        parameters = {}
+        parameters["model"] = model_object["slug"]
+        messages = [{"role": "user", "content": "Hello, how is it going?"}]
+        messages.append({"role": "system", "content": "Behave like Rick Sanchez."})
+        parameters["messages"] = messages
+
+        print(f"Testing model {model_object['slug']} from {connector['provider']} connector \n\n\n")
+        response = connector_object.chat_completion(stream=False, **parameters)
+        print(f"\n\n\n Model {model_object['slug']} succeeed 🚀 \n\n\n")
+
+        response = connector_object.chat_completion(stream=True, **parameters)
+        for text in response:
+            print(connector_object.parse_chunk(text))
+        print(f"\n\n\n Model {model_object['slug']} succeeed with streaming 🚀 \n\n\n")
+
+    if len(text2image_models) > 0:
+        model_object = text2image_models[0]
+
+        parameters = {}
+        parameters["model"] = model_object["slug"]
+        parameters["prompt"] = "A cute baby sea otter"
+        parameters["size"] = "1024x1024"
+        parameters["n"] = 1
+
+        print(f"Testing model {model_object['slug']} from {connector['provider']} connector \n\n\n")
+        response = connector_object.generate_image(**parameters)
+        print(f"\n\n\n Model {model_object['slug']} succeeed 🚀 \n\n\n")
+
+    if len(text2vector_models) > 0:
+        model_object = text2vector_models[0]
+
+        input = "Hello, how is it going?"
+        print(f"Testing model {model_object['slug']} from {connector['provider']} connector")
+        response = connector_object.embeddings(model=model_object["slug"], input=input)
+        print(f"Embeddings: {len(response['data'][0])}")
+        print(f"Model {model_object['slug']} succeeed 🚀 \n\n\n")
+
+
+def run_all_connectors(connector_name_list: list[str] | None = None) -> None:
+    if connector_name_list is None:
+        connectors_mapping = load_models_file()["connectors"]
+        connector_name_list = [connector["provider"] for connector in connectors_mapping]
+
+    for connector in connector_name_list:
+        print("=" * 20, f"Running for Connector: {connector}", "=" * 20)
+        run_single_connector(connector_name=connector)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Testing different providers from prem-utils package")
+
+    parser.add_argument(
+        "name",
+        nargs="+",
+        help="Name of the connector/s. If you type 'all', the testing will be done for all the providers",
+    )
+
+    args = parser.parse_args()
+
+    if args.name == "all":
+        run_all_connectors()
+    else:
+        run_all_connectors(connector_name_list=args.name)
